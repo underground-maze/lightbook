ec5769ccb330625ab3eb57872b2d5282d3543af6
Vasiliy Zemlyanov <angeldaren@gmail.com>
2014-06-06 21:53:39 +0400

    Add m2m relation between authors and books. Example

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606121714) do
+ActiveRecord::Schema.define(version: 20140606172857) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "authors_books", id: false, force: true do |t|
+    t.integer "author_id"
+    t.integer "book_id"
   end
 
   create_table "books", force: true do |t|
